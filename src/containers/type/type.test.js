1cf4f098f1fdb9bbb73e33911075039ac9a4f2e8
saitelabd <saitelabd@sii-maroc.com>
2021-04-26 08:47:08 +0000

    remove unused Files

--- src/containers/type/type.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Type from './type';
-
-test('renders learn react link', () => {
-  render(<Type />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
--- src/containers/viewPokemon/viewPokemon.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import ViewPokemon from './viewPokemon';
-
-test('renders learn react link', () => {
-  render(<ViewPokemon />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
